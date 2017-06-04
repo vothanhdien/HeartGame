66cfbfdb42e843e879a61cc91d6436a8b38add4d
Vo Thanh Dien <vtdien96@gmail.com>
2017-06-04 11:08:43 +0700

    commit some object

---
+++ Object/src/Object/Player.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Object;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ *
+ * @author didim
+ */
+public class Player {
+    
+    String name;
+    ArrayList<Card> hand = new ArrayList<>();
+    int score;
+    
+    public Player(String name){
+        score = 0;
+        this.name = name;
+    }
+    
+    //----------------
+    void addCard(Card newCard){
+        hand.add(newCard);
+    }
+    void sortHand(){ hand.sort(new Comparator<Card>() {
+        @Override
+        public int compare(Card t, Card t1) {
+            return t.compareTo(t1);
+        }
+    });}
+    
+    boolean checkType(CardType type){
+        return hand.stream().anyMatch((c) -> (c.getType() == type));
+    }
+    
+    //----------getter, setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int score) {
+        this.score += score;
+    }
+    
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    
+}
---
+++ Object/src/Object/Value.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Object;
+
+/**
+ *
+ * @author didim
+ */
+public enum Value{TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NIGHT,TEN,JACK,QUEEN,KING,ACE};
