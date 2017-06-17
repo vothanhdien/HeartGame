20bf1aed9ae7560c88b37e1cc8056ab7765d078f
tmpdat <tmpdat1206@gmail.com>
2017-06-17 14:09:51 +0700

    Tăng thời gian pick bài lên 1.5s

--- Server/src/server/ServerHearts.java
+++ Server/src/server/ServerHearts.java
@@ -407,7 +407,7 @@ public class ServerHearts {
             return c;
         } else {
             try {
-                Thread.sleep(0);
+                Thread.sleep(1500);
             } catch (InterruptedException ex) {
                 Logger.getLogger(ServerHearts.class.getName()).log(Level.SEVERE, null, ex);
             }
